4de82f0712177156ca5e62c6e70c273263b2e5f3
Stanislav <128305902+CrackBrainPlay@users.noreply.github.com>
2024-05-13 21:23:09 +0300

    update 1 button

--- src/components/Users/UsersOld.jsx
+++ src/components/Users/UsersOld.jsx
@@ -31,6 +31,7 @@ const UsersOld = (props) => {
                 <span>
                     <span>
                         {/* <div>{u.firstName}&nbsp;{u.lastName}</div> */}
+                        {/* <div>{u.firstName}&nbsp;{u.lastName}</div> */}
                         <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
